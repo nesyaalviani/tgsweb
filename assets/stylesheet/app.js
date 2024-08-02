5d96d7a9c7bad90d1d21acc97d3f5108f4a3287a
nesyaalviani <nesya31120com@gmail.com>
2024-08-02 22:14:54 +0700

    Delete assets directory

--- assets/stylesheet/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".nav");
-
-  if (window.scrollY >= 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
